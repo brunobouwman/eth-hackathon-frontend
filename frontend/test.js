2fb07e1e174fb9432a48584d7884006b352f4ab4
bruno.bouwman <bruno.bouwman@ceiia.com>
2023-11-04 11:05:46 +0000

    initial commit

---
+++ frontend/test.js
@@ -0,0 +1,11 @@
+let test = "This is a test.";
+
+return (
+  <>
+    <div class="container border border-info p-3 text-center">
+      <h1>Hello {props.surname}</h1>
+
+      <p> {test} </p>
+    </div>
+  </>
+);
